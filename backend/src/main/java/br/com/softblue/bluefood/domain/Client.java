b70a2183b76adede07ce7867e237df6a01c6e840
LeonardoMFSilva <leo_leb5@hotmail.com>
2021-07-16 08:00:28 -0300

    Adding validations to client and user classes

--- backend/src/main/java/br/com/softblue/bluefood/domain/Client.java
+++ backend/src/main/java/br/com/softblue/bluefood/domain/Client.java
@@ -4,7 +4,10 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @Setter
@@ -12,6 +15,13 @@ import javax.persistence.Entity;
 @Entity
 public class Client extends User{
 
+    @NotBlank(message = "O CPF não pode ser vazio!")
+    @Pattern(regexp = "[0-9] {10,11}", message = "O CPF possui formato inválido!")
+    @Column(length = 11)
     private String cpf;
+
+    @NotBlank(message = "O CEP não pode ser vazio!")
+    @Pattern(regexp = "[0-9] {8}", message = "O CEP possui formato inválido!")
+    @Column(length = 8)
     private String cep;
 }
--- backend/src/main/java/br/com/softblue/bluefood/domain/User.java
+++ backend/src/main/java/br/com/softblue/bluefood/domain/User.java
@@ -4,10 +4,15 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 @Getter
@@ -20,8 +25,22 @@ public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @NotBlank(message = "Nome não pode ser vazio!")
+    @Size(max = 80, message = "Nome muito grande")
     private String nome;
+
+    @NotBlank(message = "E-mail não pode ser vazio!")
+    @Size(max = 60, message = "O e-mail informado é muito grande!")
+    @Email(message = "E-mail inválido!")
     private String email;
+
+    @NotBlank(message = "Senha não pode ser vazia!")
+    @Size(max = 80, message = "Senha muito longa!")
     private String senha;
+
+    @NotBlank(message = "Telefone não pode ser vazio!")
+    @Pattern(regexp = "[0-9] {10,11}", message = "Telefone inválido!")
+    @Column(length = 11, nullable = false)
     private String telefone;
 }
